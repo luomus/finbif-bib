ba7a3427b3d4bf470fade2b77b967ebd2966d2d0
Will <willi@mmorris.email>
2020-11-18 15:30:39 +0200

    Ensure private key has correct permissions

--- update.sh
+++ update.sh
@@ -3,6 +3,7 @@ echo "bibuser:x:$(id -u):0:bibuser user:/home/bibuser:/sbin/nologin" >> /etc/pas
 git config --global user.email $GIT_EMAIL
 git config --global user.name $GIT_USER
 cp -p keys/id_ed25519 .ssh/id_ed25519
+chmod 0600 .ssh/id_ed25519
 git clone git@github.com:luomus/finbif-bib
 cd finbif-bib
 Rscript --verbose update-bib.R
